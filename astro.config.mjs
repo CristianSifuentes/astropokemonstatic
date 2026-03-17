fbf614efef225dc371f138be7c19ec5165bb60ef
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-03-16 19:44:50 -0600

    Working with JavaScript and TypeScript Scripts in Astro Components, LocalStorage and its Potential Drawbacks, Islands, Integrations, SolidJS as a UI Framework, Signals, Props, Children, Show, For Vanilla JavaScript, Persisting State Between Navigation, Videos, Island State, Prop State, Sending Astro Components to Islands, Icons And More

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import solid from '@astrojs/solid-js';
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [solid(), icon()],
   vite: {
     plugins: [tailwindcss()]
   }
